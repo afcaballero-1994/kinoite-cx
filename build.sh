0766d28cc058c5b46fa68de080f69f9808f302eb
Andres <59713551+afcaballero-1994@users.noreply.github.com>
2025-02-06 22:02:36 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,11 +11,8 @@ set -ouex pipefail
 
 rpm-ostree install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-rpm-ostree install -y ffmpeg
-
-
-rpm-ostree install -y kitty tmux distrobox docker emacs ffmpeg fish gstreamer1-plugin-openh264 gstreamer1-plugins-ugly kitty kmod-nvidia \
-                           libavcodec-freeworld libva-nvidia-driver neovim smplayer x265 \
+rpm-ostree install -y kitty tmux distrobox docker emacs ffmpeg fish gstreamer1-plugin-openh264 gstreamer1-plugins-ugly kitty \
+                           libva-nvidia-driver neovim smplayer x265 \
                            zsh
 
 # Use a COPR Example:
