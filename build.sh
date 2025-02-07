0fed7166ba1f16d295313a153609bff0758c0648
Andres <59713551+afcaballero-1994@users.noreply.github.com>
2025-02-06 21:31:35 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ set -ouex pipefail
 dnf install -y --nogpgcheck https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(rpm -E %rhel).noarch.rpm
 dnf install -y --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm -E %rhel).noarch.rpm
 
+dnf install epel-release dnf-utils -y
 
 dnf install -y ffmpeg --allowerasing
 dnf install -y distrobox docker emacs fish kitty \
